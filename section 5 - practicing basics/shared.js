9493c789e8be8129d8e69f27b3a55ae698ba4317
panos <panagiotis@angelidakis.net>
2021-07-08 17:41:10 +0300

    Section 9 / Opening and Closing the Hamburger Menu

--- section 5 - practicing basics/shared.js
+++ section 5 - practicing basics/shared.js
@@ -2,6 +2,8 @@ const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
 const selectPlanButtons = document.querySelectorAll('.plan button');
 const modalNoButton = document.querySelector('.modal__action--negative');
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
 
 for (let i = 0; i< selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function() {
@@ -10,10 +12,19 @@ for (let i = 0; i< selectPlanButtons.length; i++) {
     });
 }
 
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', function() {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
+
 modalNoButton.addEventListener('click', closeModal);
 
 function closeModal() {
     backdrop.style.display = 'none';
     modal.style.display = 'none';
 }
+
+toggleButton.addEventListener('click', function() {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+})
